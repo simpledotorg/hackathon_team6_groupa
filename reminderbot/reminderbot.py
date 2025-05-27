66554362125af84a10927755be9c6c59e4d475f7
Arnaud DEMARCQ <ademarcq@RTSL-N7VH6K2VP9.local>
2025-05-27 10:23:42 +0200

    quick refactoring

--- reminderbot/reminderbot.py
+++ reminderbot/reminderbot.py
@@ -14,6 +14,31 @@ class Patient:
     sex = None
     prefered_language= "English"
     facilityName= None
+    def dumpPatient(self) -> str:
+        return jsonpickle.encode(self)
+
+
+class Message:
+    author:str
+    messageText:str
+    def __init__(self, author, messageText):
+        self.author= author
+        self.messageText=messageText
+
+
+class Conversation:
+    patient: Patient
+    messages= []
+    def __init__(self, patient):
+        self.patient= patient
+    def dumpConversation(self) -> str:
+        returnString= ''
+        for currentMessage in self.messages:
+            returnString = returnString + currentMessage.author +  ': '  +  currentMessage.messageText + '\n '
+        return returnString
+            
+
+
 
 ###
 ## Connects to Google Gemini. TODO: make it an interface
@@ -24,7 +49,7 @@ client = genai.Client(api_key=os.environ.get('GEMINI_API_KEY'))
 ## This loads the template for the prompt
 ###
 with open("./prompt.base.txt", 'r') as file:
-    file_content = file.read()
+    discussion_prompt_file_content = file.read()
 
 
 ###
@@ -36,16 +61,31 @@ test_patient.age = 43
 test_patient.sex= "male"
 test_patient.prefered_language="English"
 test_patient.facilityName="Test PHC"
+currentConversation = Conversation(test_patient);
+
+
+###
+## Defines a few helper functions
+###
+def getDiscussionPrompt() -> str:
+    returnString: str =  discussion_prompt_file_content.replace("__PATIENT_DATA__", test_patient.dumpPatient())
+    returnString = returnString.replace("__CONVERSATION__", currentConversation.dumpConversation())
+    return returnString
 
 ###
 ## Generate customized first prompt for the patient
 ###
-currentPrompt = file_content.replace("__PATIENT_DATA__", jsonpickle.encode(test_patient))
+#currentPrompt = file_content.replace("__PATIENT_DATA__", test_patient.dumpPatient())
+
+
+
+    
+
+currentPrompt = getDiscussionPrompt()
 
 ###
 ## Loops until satisfied. Max is 5
 ###
-
 for i in range(5):
     ###
     ## Gets the message to be sent to the patient
@@ -53,17 +93,18 @@ for i in range(5):
     chatbot_message = client.models.generate_content(
         model="gemini-2.0-flash", contents=currentPrompt
     )
-
+    currentConversation.messages.append( Message ('Bot', chatbot_message.text))
     ###
     ## Gets the answer from the patient 
     ###
     print(chatbot_message.text)
     patient_response = input()
+    currentConversation.messages.append( Message ('Patient', patient_response))
 
     ###
     ## Prepares the message for next round
     ###
-    currentPrompt = currentPrompt + "HISTORY of Message: \n " + "BOT: " + chatbot_message.text +" \nPATIENT: "+ patient_response
+    currentPrompt = getDiscussionPrompt() 
 
 
 
